e4b86841b0ec83e0778f7fb2d0c9348835d0a18d
Barış Esen <barisesennn@gmail.com>
2018-04-29 15:37:48 +0300

    camera capture added

---
+++ face_detect_camera.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import cv2
+import imageio
+
+face_cascade = cv2.CascadeClassifier('data/haarcascade-frontalface-default.xml')
+eye_cascade = cv2.CascadeClassifier('data/haarcascade-eye.xml')
+
+def detect(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
+
+
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    canvas = detect(frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
